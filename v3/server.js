bddeb675f15a9846a1b30b27474e745b7d0b2d0f
Abhishek <awittlegupta@gmail.com>
2017-09-22 21:23:20 +0000

    CSS file added

--- v3/server.js
+++ v3/server.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname+ '/bower_components'));
 
 // default view engine so we don't need to constantly type ".ejs"
