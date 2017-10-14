f49894c12ac8ca2b4a1154e55394f2a9a24d4c17
joshua <holland_74@hotmail.com>
2017-10-14 10:54:57 +0000

    fixed partials + minor tweaks to app-page & home

---
+++ public/js/header.js
@@ -0,0 +1,13 @@
+ $(window).scroll(function() {
+    // declare variable
+    var topPos = $(this).scrollTop();
+    // if user scrolls down - show scroll to top button
+    if (topPos < 5) {
+        $(".navbar").css("background-color", "rgba(204, 255, 255, .5)");
+        $(".navbar-brand a, #myNavbar ul li *").css("color", "black");
+    } 
+    else {
+        $(".navbar").css("background-color", "white");
+        $(".navbar-brand a, #myNavbar ul li *").css("color", "#808080");
+    }
+});
